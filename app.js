c04f71438dece6d2af376029814a473790cf22ae
shivvamm <shivam574488@gmail.com>
2022-12-17 04:58:05 +0530

    completed the base part

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 var express =require("express");
+var bodyParser = require("body-parser");
 var app=express();
-
 var PORT=3000;
 
 app.set("view engine","ejs");
+
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+var i1=[];
+var i;
 app.get("/",(req,res)=>{
 //res.send("Hello");
-res.render("list");
+res.render("list",{newListItems:i1});
 })
 
+app.post("/",(req,res)=>{
+    i=req.body.n;
+    i1.push(i);
+    //console.log(i);
+    //res.render("list",{newListItem:i})
+    res.redirect("/");
+})
 
 app.listen(3000,function(){
     console.log("Listening on Port 3000");
